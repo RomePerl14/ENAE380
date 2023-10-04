df59c761d3fa511755958dc98302050f81a84b7a
Romeo-Perl-14 <romeoperlstein14@gmail.com>
2023-10-03 23:37:30 -0400

    balling

---
+++ Lab3/lab3.py
@@ -0,0 +1,102 @@
+"""
+This is your template for lab3. Implement all questions in the appropriate 
+function. Implement helper functions as needed with
+a short (one-line) description of their purpose.
+"""
+
+class logic():
+    def __init__():
+        pass
+
+    def AND_gate(bit1 : bool, bit2 : bool):
+        '''
+        Description
+        ----------
+        A classic AND logic gate
+
+        Parameters
+        ----------
+            bit1: The first input bit
+            bit2: The second input bit
+
+        Returns
+        ----------
+            True or False, depending on what the deal is
+        '''
+
+        if bit1 and bit2:
+            return True
+        else:
+            return False
+        
+        
+    def OR_gate(bit1 : bool, bit2 : bool):
+        '''
+        Description
+        ----------
+        A classic OR logic gate
+
+        Parameters
+        ----------
+            bit1: The first input bit
+            bit2: The second input bit
+
+        Returns
+        ----------
+            True or False, depending on what the deal is
+        '''
+
+        if bit1 or bit2:
+            return True
+        else:
+            return False
+    
+        
+
+
+
+
+
+def two_bit(list1, list2):
+    """
+    
+    Parameters
+    ----------
+
+    list1: list(bits)
+    list2: list(bits)
+
+
+    Returns
+    -------
+    list  
+    """
+    pass 
+
+def conversion():
+    """
+    
+    """
+    # replace the line below with your implementation
+    pass
+
+def two2dec():
+    """
+    
+    """
+    # replace the line below with your implementation
+    pass
+
+def dec2float():
+    """
+    
+    """
+    # replace the line below with your implementation
+    pass
+
+def float2dec():
+    """
+    
+    """
+    # replace the line below with your implementation
+    pass
